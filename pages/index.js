f0fcb0464ac00aec84d10b83500e203eff04500c
antonshurukov22 <118719520+antonshurukov22@users.noreply.github.com>
2022-11-28 14:21:00 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,4 +4,6 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-<script src="https://commerce5.maxoptra.ru/gt/gt-api/website-widget?p=https://commerce5.maxoptra.ru/gt/&a=shurukov&l=ru&w=500&h=500" charset="utf-8" id="tracking-widget-script"></script>}
+<script src="https://commerce5.maxoptra.ru/gt/gt-api/website-widget?p=https://commerce5.maxoptra.ru/gt/&a=shurukov&l=ru&w=500&h=500" charset="utf-8" id="tracking-widget-script"></script>
+  )
+}
