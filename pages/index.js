e493f7ca12151b50b4f6db68bdbc1152f0435e81
antonshurukov22 <118719520+antonshurukov22@users.noreply.github.com>
2022-11-28 11:16:12 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,14 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
+        <script src="https://commerce5.maxoptra.ru/gt/gt-api/website-widget?p=https://commerce5.maxoptra.ru/gt/&a=shurukov&l=ru&w=500&h=500" charset="utf-8" id="tracking-widget-script"></script>
+
         <link rel="icon" href="/favicon.ico" />
  <meta name="mailru-domain" content="3rNuhknz9U4gPcl5" />
       </Head>
 
       <main>
         <Header title="asdasd2efr!" />
-    <script src="https://commerce5.maxoptra.ru/gt/gt-api/website-widget?p=https://commerce5.maxoptra.ru/gt/&a=shurukov&l=ru&w=500&h=500" charset="utf-8" id="tracking-widget-script"></script>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
