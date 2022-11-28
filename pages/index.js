a6a3ee6affbc54b717688d81fc119c3b17e70b92
antonshurukov22 <118719520+antonshurukov22@users.noreply.github.com>
2022-11-28 12:00:44 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,27 +4,8 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-    
 
- <meta name="mailru-domain" content="3rNuhknz9U4gPcl5" />
-      </Head>
-
-      
-
-      <main>
-        <Header title="asdasd2efr!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
     <script src="https://commerce.maxoptra.ru/gt/gt-api/website-widget?p=https://commerce.maxoptra.ru/gt/&a=comsupporttest&l=ru&w=500&h=500" charset="utf-8" id="tracking-widget-script"></script>
-      </main>
-  
-      <Footer />
-    </div>
-   
 
   )
 }
