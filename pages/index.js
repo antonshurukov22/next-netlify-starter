63f341995a48c3bbc8fa4ffdc734fc7cdfa17ead
antonshurukov22 <118719520+antonshurukov22@users.noreply.github.com>
2022-11-28 11:41:42 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
  <meta name="mailru-domain" content="3rNuhknz9U4gPcl5" />
       </Head>
-<Body>
+<body>
         <script src="https://commerce5.maxoptra.ru/gt/gt-api/website-widget?p=https://commerce5.maxoptra.ru/gt/&a=shurukov&l=ru&w=200&h=200" charset="utf-8" id="tracking-widget-script"></script>
 
       <main>
@@ -20,8 +20,6 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-    
-    <script src="https://commerce5.maxoptra.ru/gt/gt-api/website-widget?p=https://commerce5.maxoptra.ru/gt/&a=shurukov&l=ru&w=200&h=200" charset="utf-8" id="tracking-widget-script"></script>
     
       </main>
 </Body>
