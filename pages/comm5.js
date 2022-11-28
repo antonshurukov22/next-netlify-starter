e7b532a7189c92927bd00fece306cfe7f2b954c8
antonshurukov22 <118719520+antonshurukov22@users.noreply.github.com>
2022-11-28 12:18:47 +0400

    Create comm5.js

---
+++ pages/comm5.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+
+<script src="https://commerce5.maxoptra.ru/gt/gt-api/website-widget?p=https://commerce5.maxoptra.ru/gt/&a=shurukov&l=ru&w=500&h=500" charset="utf-8" id="tracking-widget-script"></script>  )
+}
