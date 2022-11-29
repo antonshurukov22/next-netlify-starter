1a576a366b222b6f874e04a8e3299043ad70ba54
antonshurukov22 <118719520+antonshurukov22@users.noreply.github.com>
2022-11-29 12:07:07 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,27 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-<script src="https://commerce5.maxoptra.ru/gt/gt-api/website-widget?p=https://commerce5.maxoptra.ru/gt/&a=shurukov&l=ru&w=500&h=500" charset="utf-8" id="tracking-widget-script"></script>
+	<script>
+(function () {
+  var trackingIdArr = document.URL.match(/trackingid=([^&]+)/);
+ var frameSrc = 'https://lpl5.com/index.html?a=shurukov&l=ru&w=500&h=500&s=shurukov.maxoptra.com&e=0';
+  if (trackingIdArr) {
+       var trackingId = trackingIdArr[1];
+      if (trackingId) {
+          frameSrc += '&trackingid=' + trackingId;
+      }
+  }
+   var frame = document.createElement('iframe');
+    frame.src = frameSrc;
+    frame.id = 'tracking-widget';
+    frame.scrolling = 'no';
+    frame.width = '500';
+    frame.height = '500';
+    frame.frameBorder = '0';
+    var scriptTag = document.querySelector('#maxoptra-widget-script,#tracking-widget-script');
+    scriptTag.parentNode.insertBefore(frame, scriptTag.nextSibling);
+})();
+</script> 
+
   )
 }
